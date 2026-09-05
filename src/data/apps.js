e67bee6e0a9366f679703ca677fa1391ac6ce037
Evelyn Rodriguez <errodriguez0703@gmail.com>
2026-09-04 21:20:35 -0700

    slowly adding icons

--- src/data/apps.js
+++ src/data/apps.js
@@ -1,9 +1,14 @@
+import folderIcon from '../assets/imgs/Folder_01.ico'
+import computerIcon from '../assets/imgs/Computer01.ico'
+import experienceIcon from '../assets/imgs/Bag01.ico'
+
 export const desktopApps = [
-  { id: 'about', label: 'about me', icon: '📄' },
-  { id: 'experience', label: 'experience', icon: '💼' },
-  { id: 'music', label: 'music player', icon: '🎵' },
-  { id: 'anime', label: 'anime list', icon: '📺' },
-  { id: 'movies', label: 'movie list', icon: '🎬' },
-  { id: 'mtg', label: 'mtg tracker', icon: '🃏' },
-  { id: 'contact', label: 'contact', icon: '✉️' },
+    {id: 'explorer', label: 'Eve Explorer', icon: '📁', img: folderIcon},
+    { id: 'about', label: 'about me', icon: '📄', img: computerIcon },
+    { id: 'experience', label: 'experience', icon: '💼', img: experienceIcon },
+    { id: 'music', label: 'music player', icon: '🎵', img: null },
+    { id: 'anime', label: 'anime list', icon: '📺', img: null },
+    { id: 'movies', label: 'movie list', icon: '🎬', img: null },
+    { id: 'mtg', label: 'mtg tracker', icon: '🃏', img: null },
+    { id: 'contact', label: 'contact', icon: '✉️', img: null },
 ]
